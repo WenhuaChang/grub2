8ecebafe1ac75f4091ce1c4521a8deaa0d44826f
Michael Chang <mchang@suse.com>
2022-01-17 17:45:00 +0800

    Add grub_envblk_buf helper function
    This helps in creation and initialization of memory buffer for
    environment block of given size.
    
    Signed-off-by: Michael Chang <mchang@suse.com>

--- util/grub-editenv.c
+++ util/grub-editenv.c
@@ -255,9 +255,7 @@ create_env_on_block (void)
   if (fp == NULL)
     grub_util_error (_("cannot open `%s': %s"), device, strerror (errno));
 
-  buf = xmalloc (size);
-  memcpy (buf, GRUB_ENVBLK_SIGNATURE, sizeof (GRUB_ENVBLK_SIGNATURE) - 1);
-  memset (buf + sizeof (GRUB_ENVBLK_SIGNATURE) - 1, '#', size - sizeof (GRUB_ENVBLK_SIGNATURE) + 1);
+  buf = grub_envblk_buf (size);
 
   if (fseek (fp, offset, SEEK_SET) < 0)
     grub_util_error (_("cannot seek `%s': %s"), device, strerror (errno));
