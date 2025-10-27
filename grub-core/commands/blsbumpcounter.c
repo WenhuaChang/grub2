e358d990ef6ee2e58e7262372d333996449c79f9
Michael Chang <mchang@suse.com>
2025-10-27 16:06:35 +0800

    grub2-bls-boot-counting
    Add a new bls_bumpcunter grub command that implement boot counting for bls entries.
    
    Boot counting, explained in systemd Automatic Boot Assessment, keep track of the
    avaiable tries for each entry and the number of attempted boot. The bls_bumpcunter
    command parse the entry id, check if there is a boot count enabled and update
    its value accordingly. Then, EFI_SIMPLE_FILE_SYSTEM_PROTOCOL is used to rename
    the entry on the EFI partition.
    
    https://systemd.io/AUTOMATIC_BOOT_ASSESSMENT/

---
+++ grub-core/commands/blsbumpcounter.c
@@ -0,0 +1,54 @@
+/*-*- Mode: C; c-basic-offset: 2; indent-tabs-mode: t -*-*/
+
+/* blsbumpcounter.c - implementation of boot counting for the Automatic Boot Assessment */
+
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <grub/extcmd.h>
+#include <grub/dl.h>
+
+#include <stddef.h>
+
+GRUB_MOD_LICENSE ("GPLv3+");
+
+
+static grub_err_t
+grub_cmd_bumpcounters (grub_extcmd_context_t ctxt UNUSED,
+         int argc UNUSED, char **args UNUSED)
+{
+    /* placeholder, as blsbumpcounter only work on EFI platforms */
+  return GRUB_ERR_NONE;
+}
+
+
+static grub_extcmd_t cmd;
+
+GRUB_MOD_INIT(blsbumpcounter)
+{
+  cmd = grub_register_extcmd ("bls_bumpcounter",
+                  grub_cmd_bumpcounters,
+                  0,
+                  NULL,
+                  N_("Bump the boot entry counting (only works on EFI)."),
+                  NULL);
+}
+
+GRUB_MOD_FINI(blsbumpcounter)
+{
+  grub_unregister_extcmd (cmd);
+}
