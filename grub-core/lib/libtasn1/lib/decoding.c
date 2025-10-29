6a38608fcf50c6be737c0d728e574490c2736625
Michael Chang <mchang@suse.com>
2025-10-29 14:58:14 +0800

    Revert "libtasn1: disable code not needed in grub"
    This reverts commit a51670f2edc9b6aefdce35eb723cc255073086f4.

--- grub-core/lib/libtasn1/lib/decoding.c
+++ grub-core/lib/libtasn1/lib/decoding.c
@@ -1620,7 +1620,6 @@ asn1_der_decoding (asn1_node * element, const void *ider, int ider_len,
   return asn1_der_decoding2 (element, ider, &ider_len, 0, errorDescription);
 }
 
-#if 0
 /**
  * asn1_der_decoding_element:
  * @structure: pointer to an ASN1 structure
@@ -1651,7 +1650,6 @@ asn1_der_decoding_element (asn1_node * structure, const char *elementName,
 {
   return asn1_der_decoding (structure, ider, len, errorDescription);
 }
-#endif
 
 /**
  * asn1_der_decoding_startEnd:
--- grub-core/lib/libtasn1/lib/errors.c
+++ grub-core/lib/libtasn1/lib/errors.c
@@ -57,8 +57,6 @@ static const libtasn1_error_entry error_algorithms[] = {
   {0, 0}
 };
 
-
-#if 0
 /**
  * asn1_perror:
  * @error: is an error returned by a libtasn1 function.
@@ -75,7 +73,6 @@ asn1_perror (int error)
   const char *str = asn1_strerror (error);
   fprintf (stderr, "LIBTASN1 ERROR: %s\n", str ? str : "(null)");
 }
-#endif
 
 /**
  * asn1_strerror:
